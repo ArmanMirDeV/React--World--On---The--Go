582514676a1ff507d1b6caef9a4e6652fc2abc00
Arman Mir <mirarman8583@gmail.com>
2025-09-20 12:14:04 +0600

    First Commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,28 @@
+/* function Countries(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+//-----------------------
+
+const Countries = () =>{
+    return(
+        <div> </div>
+    )
+} */
+
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>In The Countries...</h1>
+        </div>
+    );
+};
+
+export default Countries;
